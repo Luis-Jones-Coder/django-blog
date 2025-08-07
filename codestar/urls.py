055352094adb55c45aa4079175f80bcf5ba01127
Luis-Jones-Coder <n16e3tnn@students.codeinstitute.net>
2025-08-07 03:20:58 +0100

    readies code for deploy

--- codestar/urls.py
+++ codestar/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for blog project.
+URL configuration for codestar project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -16,7 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from blog.views import my_blog
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+   path('blog/', my_blog, name='blog'),
 ]
+    
